97147062c63ab064296359b6e20848e114c043fb
Madhav Dodiya <105213289+MadhavDodiya@users.noreply.github.com>
2026-02-05 12:03:05 +0530

    `Refactor code and remove unused files` and complete projects

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -27,7 +27,7 @@ router.post("/create", async (req, res) => {
 // GET USERS (pagination + search)
 router.get("/", protect, async (req, res) => {
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 5;
+  const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
   const search = req.query.search || "";
 
--- Backend/server.js
+++ Backend/server.js
@@ -30,3 +30,4 @@ app.use(errorHandler);
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
+  
